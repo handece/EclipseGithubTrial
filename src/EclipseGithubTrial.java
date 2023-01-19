608ce2f1a35bd14aa9b136ac77cf954abb20b719
handece <handesarikaya94@gmail.com>
2023-01-19 13:58:07 +0300

    Syso message changed

--- src/EclipseGithubTrial.java
+++ src/EclipseGithubTrial.java
@@ -3,7 +3,7 @@ public class EclipseGithubTrial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Eclipse github trial!!");
+		System.out.println("Eclipse github trial second version!!");
 	}
 
 }
